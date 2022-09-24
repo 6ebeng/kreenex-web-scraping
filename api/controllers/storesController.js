3ce30cd12a08184d74aaca0c868a77cadce17173
r3kbin <rekbin.devs@gmail.com>
2022-09-24 16:43:49 +0300

    fix

--- api/controllers/storesController.js
+++ api/controllers/storesController.js
@@ -265,8 +265,9 @@ puppeteer.use(proxyRouter)
       let requiredSize = Size.toString();
       var NotInStockSizes = await elementSelector(page,data.notInStockSizes.selector,data.notInStockSizes.attribute || null,data.notInStockSizes.regex || null,data.notInStockSizes.groups || [],true)
         var isOutStock
+        console.log("here is " + NotInStockSizes)
         await NotInStockSizes.foreach(item=>{
-          console.log("here is " + NotInStockSizes[item])
+          
           if(NotInStockSizes[item].trim() === requiredSize.trim()) isOutStock = true
         })
         if (isOutStock) {
