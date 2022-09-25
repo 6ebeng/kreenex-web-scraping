6771f89ae80edda94b5f77142a4326d8630daf32
r3kbin <rekbin.devs@gmail.com>
2022-09-26 02:36:53 +0300

    fix

--- api/controllers/storesController.js
+++ api/controllers/storesController.js
@@ -249,7 +249,7 @@ puppeteer.use(proxyRouter)
       
     });
 
-    //if(data.isHeadless) await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33");
+    if(data.isHeadless) await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33");
 
 
     await page.goto(req.body.Url, {
