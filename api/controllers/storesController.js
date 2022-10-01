006135b281a2bf0d81205748931460b1d7c9d98d
r3kbin <rekbin.devs@gmail.com>
2022-10-01 09:25:28 +0300

    fix

--- api/controllers/storesController.js
+++ api/controllers/storesController.js
@@ -309,7 +309,7 @@ puppeteer.use(proxyRouter)
       
       Object.defineProperty(navigator, "language", {get: () => "en-US" });     
       Object.defineProperty(navigator, "languages", {get: () => ['en-US', 'en', 'ku'] });
-      Object.defineProperty(navigator, 'webdriver', {get: () => false });
+      //Object.defineProperty(navigator, 'webdriver', {get: () => true });
       Object.defineProperty(navigator, 'deviceMemory', {get: () => 8  });
       Object.defineProperty(navigator, 'hardwareConcurrency', {get: () => 8});
       Object.defineProperty(navigator, 'platform', {get: () => 'Win32'  });
