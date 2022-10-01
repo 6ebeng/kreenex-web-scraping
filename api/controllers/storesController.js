d52802604b80a8f2e92bf47414cdb3e43d221c35
r3kbin <rekbin.devs@gmail.com>
2022-10-01 11:28:02 +0300

    fix

--- api/controllers/storesController.js
+++ api/controllers/storesController.js
@@ -289,6 +289,22 @@ const data = require('../models/data/' + store)
       Object.defineProperty(navigator, 'deviceMemory', {get: () => 8  });
       Object.defineProperty(navigator, 'hardwareConcurrency', {get: () => 8});
       Object.defineProperty(navigator, 'platform', {get: () => 'Win32'  });
+      Object.defineProperty(navigator, 'plugins', {get: function() {return [1, 2, 3, 4, 5];}});
+
+      const getParameter = WebGLRenderingContext.getParameter;
+      WebGLRenderingContext.prototype.getParameter = function (parameter) {
+        // UNMASKED_VENDOR_WEBGL
+        if (parameter === 37445) {
+          return 'Intel Open Source Technology Center';
+        }
+        // UNMASKED_RENDERER_WEBGL
+        if (parameter === 37446) {
+          return 'Mesa DRI Intel(R) Ivybridge Mobile ';
+        }
+
+        return getParameter(parameter);
+      };
+
       // (function () {        var overwrite = function (name) {
       //     const OLD = HTMLCanvasElement.prototype[name];
       //     Object.defineProperty(HTMLCanvasElement.prototype, name, {
