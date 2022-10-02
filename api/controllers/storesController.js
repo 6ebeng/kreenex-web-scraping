12f6862f392c4a307a0ae6cb60f84387632a9f08
r3kbin <rekbin.devs@gmail.com>
2022-10-02 11:40:59 +0300

    fix

--- api/controllers/storesController.js
+++ api/controllers/storesController.js
@@ -362,7 +362,7 @@ const data = require('../models/data/' + store)
       waitUntil: data.waitUntil,
       timeout: 0
     });
-    if (data.debug) console.log(response.headers())
+    if (data.debug) console.log(response.headers().json())
 
     await page.evaluate(scrollToBottom, {frequency: 100,timing: 3});
 
