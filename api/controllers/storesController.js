9446c84f89247c9e67e5787e34f0b24ac6d69ad3
r3kbin <rekbin.devs@gmail.com>
2022-10-06 13:28:27 +0300

    fix

--- api/controllers/storesController.js
+++ api/controllers/storesController.js
@@ -212,7 +212,7 @@ async function search(req, res) {
 
   var match = await url.match("^((http[s]?|ftp):\/\/)?\/?([^\/\.]+\.)*?([^\/\.]+\.[^:\/\s\.]{1,3}(\.[^:\/\s\.]{1,2})?(:\d+)?)($|\/)([^#?\s]+)?(.*?)?(#[\w\-]+)?$")
   let store = await match[4].replace(/\..+/g, '')
-  Console.log("isValidStore >>" + await isValidStore(store))
+  console.log("isValidStore >>" + await isValidStore(store))
   if (!await isValidStore(store)) {
     return res.status(500).json({
       ResponseCode: 500,
