c7bada44652dbb9cc1349d54bd30c93edb014118
r3kbin <rekbin.devs@gmail.com>
2022-10-09 18:48:31 +0300

    fix

--- api/controllers/storesController.js
+++ api/controllers/storesController.js
@@ -323,7 +323,7 @@ async function search(req, res) {
 
     await page.setRequestInterception(true);
 
-    await useProxy(page,proxy);
+    //await useProxy(page,proxy);
     //Block unnecessary resource types and urls
     await blockResources(page,data)
     
