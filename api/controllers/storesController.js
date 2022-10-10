06fa4ac9da72c8e266771ccf5518be998f760db4
r3kbin <rekbin.devs@gmail.com>
2022-10-10 16:15:56 +0300

    fix

--- api/controllers/storesController.js
+++ api/controllers/storesController.js
@@ -324,7 +324,7 @@ async function search(req, res) {
     
     const page = (await browser.pages())[0]; 
     
-
+    await require("../helper/evasions/canvas.fingerprint")().onPageCreated(page)
 
     await require("puppeteer-extra-plugin-stealth/evasions/user-agent-override")({userAgent: userAgent,locale: 'en-US,en', platform: 'Win32',}).onPageCreated(page)
     // await require(`puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency`)(8).onPageCreated(page)
