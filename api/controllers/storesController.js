b0782c0cf631c097757fa8b8400e17ffc4808466
r3kbin <rekbin.devs@gmail.com>
2022-10-10 15:37:07 +0300

    fix

--- api/controllers/storesController.js
+++ api/controllers/storesController.js
@@ -349,7 +349,7 @@ async function search(req, res) {
 
     //await page.setUserAgent(userAgent);
 
-    console.log(await page.browser().userAgent())
+    console.log(await browser.userAgent())
 
 
     // Saved cookies reading
