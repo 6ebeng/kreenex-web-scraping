0409259bee13b1aaeedd32e3d92349c8fc44ee12
r3kbin <rekbin.devs@gmail.com>
2022-10-13 22:25:38 +0300

    fix

--- api/controllers/storesController.js
+++ api/controllers/storesController.js
@@ -334,7 +334,11 @@ async function search(req, res) {
     }).onPageCreated(page)
     await require(`puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency`)(8).onPageCreated(page)
     await require(`puppeteer-extra-plugin-stealth/evasions/navigator.vendor`)({ vendor: 'Google Inc.' }).onPageCreated(page)
-    await require(`puppeteer-extra-plugin-stealth/evasions/webgl.vendor`)({vendor: "Google Inc. (Intel)", renderer: "Intel, Intel(R) HD Graphics 4000 Direct3D11 vs_5_0 ps_5_0, D3D11","platform": "Win32"}).onPageCreated(page)
+    await require(`puppeteer-extra-plugin-stealth/evasions/webgl.vendor`)({
+      gpu: "Intel, Intel(R) HD Graphics 4000 Direct3D11 vs_5_0 ps_5_0, D3D11",
+      webgl: "Intel, Intel(R) HD Graphics 4000 Direct3D11 vs_5_0 ps_5_0, D3D11",
+      webgl2: "Intel, Intel(R) HD Graphics 4000 Direct3D11 vs_5_0 ps_5_0, D3D11",
+    }).onPageCreated(page)
     await require(`puppeteer-extra-plugin-stealth/evasions/navigator.languages`)(['en-US', 'en']).onPageCreated(page)
 
   
