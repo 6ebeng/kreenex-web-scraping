1d7519ba2697721149e4984dfd7c49f37a2a4430
r3kbin <rekbin.devs@gmail.com>
2022-10-14 21:47:29 +0300

    fix

--- api/controllers/storesController.js
+++ api/controllers/storesController.js
@@ -327,15 +327,15 @@ async function search(req, res) {
     const page = (await browser.pages())[0]; 
     
 
-    await require("puppeteer-extra-plugin-stealth/evasions/user-agent-override")({
-      userAgent: userAgent,
-      locale: 'en-US,en',
-      maskLinux: true
-    }).onPageCreated(page)
-    await require(`puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency`)(8).onPageCreated(page)
-    await require(`puppeteer-extra-plugin-stealth/evasions/navigator.vendor`)({ vendor: 'Google Inc.' }).onPageCreated(page)
-    await require(`puppeteer-extra-plugin-stealth/evasions/webgl.vendor`)({vendor: "Google Inc. (Intel)", renderer: "Intel, Intel(R) HD Graphics 4000 Direct3D11 vs_5_0 ps_5_0, D3D11","platform": "Win32"}).onPageCreated(page)
-    await require(`puppeteer-extra-plugin-stealth/evasions/navigator.languages`)(['en-US', 'en']).onPageCreated(page)
+    // await require("puppeteer-extra-plugin-stealth/evasions/user-agent-override")({
+    //   userAgent: userAgent,
+    //   locale: 'en-US,en',
+    //   maskLinux: true
+    // }).onPageCreated(page)
+    // await require(`puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency`)(8).onPageCreated(page)
+    // await require(`puppeteer-extra-plugin-stealth/evasions/navigator.vendor`)({ vendor: 'Google Inc.' }).onPageCreated(page)
+    // await require(`puppeteer-extra-plugin-stealth/evasions/webgl.vendor`)({vendor: "Google Inc. (Intel)", renderer: "Intel, Intel(R) HD Graphics 4000 Direct3D11 vs_5_0 ps_5_0, D3D11","platform": "Win32"}).onPageCreated(page)
+    // await require(`puppeteer-extra-plugin-stealth/evasions/navigator.languages`)(['en-US', 'en']).onPageCreated(page)
 
     console.log(await page.evaluate(()=>{
       return navigator.userAgentData.platform;
