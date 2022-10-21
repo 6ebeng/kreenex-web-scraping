54fe97297aaa27f4a5e9bccdf44821e3982208de
r3kbin <rekbin.devs@gmail.com>
2022-10-21 17:42:57 +0300

    fix

--- api/controllers/storesController.js
+++ api/controllers/storesController.js
@@ -265,14 +265,6 @@ async function search(req, res) {
 
   
 
-
-        
-
-      
-
-      
-      
-
     var proxy
     if (data.proxies.length > 0){
        proxy = data.proxies[Math.floor(Math.random() * data.proxies.length)]
